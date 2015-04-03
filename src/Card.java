93ab5bba556d384ba5738cdec215e88b7e8b4211
Lawrence Wong <lawrencecywong@gmail.com>
2015-04-03 17:23:27 -0400

    Seating finished. Card shuffling and deck preparing finished

---
+++ src/Card.java
@@ -0,0 +1,37 @@
+import java.awt.image.BufferedImage;
+
+public class Card {
+	private String suit;
+	private String rank;
+	private String sprite;
+	
+	public Card(){
+		suit = null;
+		rank = null;
+		sprite = null;
+	}
+
+	public void setSuit(String inSuit) {
+		suit = inSuit;
+	}
+
+	public void setRank(String inRank) {
+		rank = inRank;
+	}
+	
+	public void setSpriteURL(String inSprite) {
+		sprite = inSprite;
+	}
+
+	public String getSuit() {
+		return suit;
+	}
+
+	public String getRank() {
+		return rank;
+	}
+	
+	public String getSpriteURL() {
+		return sprite;
+	}
+}
